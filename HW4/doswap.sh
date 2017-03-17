dbbed3963e16e65bd8995133103688f6e74d347b
Ryan <rphuang@ucdavis.edu>
2017-03-17 00:18:42 -0700

    updated swap

--- HW4/doswap.sh
+++ HW4/doswap.sh
@@ -1,9 +1,12 @@
-# kill the latest image
-docker rm -f $(docker ps -a -q -l)
+# kill all except the proxy
+# docker rm -f $(docker ps -a -q -l)
+echo "Removing old images"
+docker rm -f $(docker ps -a | grep -v "ecs189_proxy" | awk '{print $1}' | grep -v "CONTAINER")
 
+echo "Starting $1"
 # Start the new image with given parameter as name
 docker run -dP --net ecs189_default --name $1 $1
 
-
+echo "Swapping images"
 # Swap given the image name
 docker exec ecs189_proxy_1 /bin/bash /bin/swap3.sh $1
